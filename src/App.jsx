2fff6e70eeb449efbffb9b0550687414af1e5fb5
Yurii-Leniv <supusman32@gmail.com>
2026-04-23 10:52:02 +0300

    dwqpjfiqwhguqw

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,23 @@
-import './App.scss';
+import React from 'react';
+import './App.scss'; 
+import { PostList } from './components/PostList/PostList';
 
 import postsFromServer from './api/posts.json';
-import commentsFromServer from './api/comments.json';
 import usersFromServer from './api/users.json';
+import commentsFromServer from './api/comments.json';
 
+const preparedPosts = postsFromServer.map(post => ({
+  ...post,
+  user: usersFromServer.find(user => user.id === post.userId),
+  comments: commentsFromServer.filter(comment => comment.postId === post.id)
+}));
 
-const preparedPosts = postsFromServer.map(post => {
-  return {
-    ...post,
-
-    user: usersFromServer.find(user => user.id === post.userId),
-
-    comments: commentsFromServer.filter(comment => comment.postId === post.id)
-  };
-});
 
 export const App = () => (
   <section className="App">
     <h1 className="App__title">Static list of posts</h1>
+
     <PostList posts={preparedPosts} />
+
   </section>
 );
